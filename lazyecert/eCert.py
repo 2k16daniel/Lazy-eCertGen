99254b7e709202f62431e946d421e205ca1c8b89
2k16daniel <2k16daniel@gmail>
2022-06-14 21:02:57 +0800

    Added Fullname parser for excel file

--- lazyecert/eCert.py
+++ lazyecert/eCert.py
@@ -1,22 +1,35 @@
 from typing import Type
-from typing_extensions import Required
 import click
+import openpyxl
 
 #Optional lists
 @click.command()
 @click.option('--config-file',type=click.File(), help='Path of your custom config file')
+@click.option('--template',type=click.File(), help='Path of your eCertificate template ( Must be in image format)')
 @click.option('--out-dir', default="output" , type=click.Path(), help='Your desired output path')
-@click.option('--excel-file', type=click.File(), help='Path of your excel file')
+@click.option('--excel-file', type=click.Path(), help='Path of your excel file')
+@click.option('--coordinate-x', type=int, help='Coordinates on the certificate where will be printing the name ')
+@click.option('--coordinate-y', type=int, help='Coordinates on the certificate where will be printing the name ')
 
-
-def cli(out_dir,excel_file,config_file):
+def cli(out_dir,excel_file,config_file,template,coordinate_x,coordinate_y):
     """Stupid Cli app for bulk e-certificate generator and deployer via email."""
     #check first if config file exists
     if config_file is None: 
         print('Config file is missing, skipping...')
     else: print(config_file)
     #load names from excel
-    excel_parser(excel_file)
+    Name = excel_parser(excel_file)
+    print(Name)
+
+
 
 def excel_parser(excel_file):
     print('fetching names from your excel file...');
+    obj = openpyxl.load_workbook(excel_file)
+    sheet = obj.active
+    Name = []
+    #calculate num of rows that has value
+    nrow_max = len([row for row in sheet if not all([cell.value == None for cell in row])])
+    for i in range(2,nrow_max):
+        Name.append(sheet.cell(row = i, column= 3).value+" "+sheet.cell(row = i, column= 4).value+" "+sheet.cell(row = i, column= 5).value)
+    return Name
